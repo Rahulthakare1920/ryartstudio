9f3b9f828c36411a379623fa9659b05ec48afbe9
Rahulthakare1920 <thakarer557@gmail.com>
2026-07-27 15:28:18 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
+const menu = document.getElementById("mobile-menu");
+const navLinks = document.querySelector(".nav-links");
 
+menu.addEventListener("click", () => {
+
+    navLinks.classList.toggle("active");
+
+    menu.classList.toggle("open");
+
+});
